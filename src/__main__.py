7fb2786a458f0a3fbec1e41357bf8e4090e32d85
BunnyMerz <brunoarmm@id.uff.br>
2024-09-26 09:18:34 -0300

    fix: silly if mistake

--- src/__main__.py
+++ src/__main__.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     pass
 
-if try_import is not None:
+if try_import is True:
     log_format = "%(message)s"
     log_handler = RichHandler(rich_tracebacks=True, omit_repeated_times=False)
     logging.basicConfig(level=logging.DEBUG, format=log_format, handlers=[log_handler])
